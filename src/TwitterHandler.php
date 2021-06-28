ba97a917ecf8998ff5099eb087652330320c0a14
alexpin <intracomof@gmail.com>
2021-06-28 17:09:24 +0700

    handler fix

--- src/TwitterHandler.php
+++ src/TwitterHandler.php
@@ -5,7 +5,9 @@ use AnyDownloader\DownloadManager\Exception\NothingToExtractException;
 use AnyDownloader\DownloadManager\Handler\BaseHandler;
 use AnyDownloader\DownloadManager\Model\FetchedResource;
 use AnyDownloader\DownloadManager\Model\ResourceItem\Image\JPGResourceItem;
+use AnyDownloader\DownloadManager\Model\ResourceItem\ResourceItemFactory;
 use AnyDownloader\DownloadManager\Model\ResourceItem\Video\MP4ResourceItem;
+use AnyDownloader\DownloadManager\Model\ResourceItem\VideoResourceItem;
 use AnyDownloader\DownloadManager\Model\URL;
 use AnyDownloader\TwitterDownloader\Exception\CanNotExtractMediaFromTwitter;
 use AnyDownloader\TwitterDownloader\Model\Attribute\TwitterAuthorAttribute;
@@ -60,33 +62,32 @@ final class TwitterHandler extends BaseHandler
             throw new CanNotExtractMediaFromTwitter($response->errors[0]->message);
         }
 
-        try {
-            $media = $response->extended_entities->media[0];
-            $preview = JPGResourceItem::fromURL(URL::fromString($media->media_url_https));
-            $resource = new TwitterVideoFetchedResource($url);
-            $resource->setImagePreview($preview);
-            $resource->addAttribute(new IdAttribute($response->id));
-            $resource->addAttribute(new TextAttribute($response->text));
-            $resource->addAttribute(TwitterAuthorAttribute::fromTwitterUserStdObj($response->user));
-
-            if ($response->entities) {
-                $resource->addAttribute(TwitterHashtagsAttribute::fromTwitterEntitiesStdObj($response->entities));
-            }
-
-            if (is_array($media->video_info->variants) && !empty($media->video_info->variants)) {
-                foreach ($media->video_info->variants as $video) {
-                    if ($video->content_type === MP4ResourceItem::MIMEType()) {
-                        $videoItem = MP4ResourceItem::fromURL(URL::fromString($video->url), $video->bitrate);
-                        $resource->addItem($videoItem);
-                    }
-                }
-                if (isset($videoItem) && $videoItem instanceof MP4ResourceItem) {
-                    $resource->setVideoPreview($videoItem);
-                }
-            }
-
-        } catch (\Throwable $exception) {
+        if (!$response->extended_entities) {
             throw new NothingToExtractException();
+        }
+
+        $media = $response->extended_entities->media[0];
+        $preview = ResourceItemFactory::fromURL(URL::fromString($media->media_url_https));
+        $resource = new TwitterVideoFetchedResource($url);
+        $resource->setImagePreview($preview);
+        $resource->addAttribute(new IdAttribute($response->id));
+        $resource->addAttribute(new TextAttribute($response->text));
+        $resource->addAttribute(TwitterAuthorAttribute::fromTwitterUserStdObj($response->user));
+
+        if ($response->entities) {
+            $resource->addAttribute(TwitterHashtagsAttribute::fromTwitterEntitiesStdObj($response->entities));
+        }
+
+        if (is_array($media->video_info->variants) && !empty($media->video_info->variants)) {
+            foreach ($media->video_info->variants as $video) {
+                if ($video->content_type === MP4ResourceItem::MIMEType()) {
+                    $videoItem = MP4ResourceItem::fromURL(URL::fromString($video->url), $video->bitrate);
+                    $resource->addItem($videoItem);
+                }
+            }
+            if (isset($videoItem) && $videoItem instanceof VideoResourceItem) {
+                $resource->setVideoPreview($videoItem);
+            }
         }
 
         return $resource;
